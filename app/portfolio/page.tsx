67ff92ebb57139303dc49ab19a33f9691d7e96d2
hallvaeb <hallvard.bjorgen@gmail.com>
2024-09-15 20:07:23 +0200

    update portfolio work in progress text

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,7 +3,7 @@ export default function NewSite() {
     return (
       <div className="p-6">
         <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-bold">Portfolio (not yet implemented.)</h1>
+          <h1 className="text-2xl font-bold">Portfolio not implemented</h1>
         </div>
       </div>
     );
