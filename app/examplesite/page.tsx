7e7dd54bc84f658665b20e216dcb753ed22e62ab
hallvaeb <hallvard.bjorgen@gmail.com>
2024-10-03 22:09:31 +0200

    example new image page

---
+++ app/examplesite/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function ExampleSite() {
+  return (
+    // Show image on entire screen
+    <div className="flex w-full">
+      <Image
+        src="/example_site.png"
+        alt="Example Site"
+        priority={true}
+        // object-fit="contain"
+        layout="responsive"
+        width={5000}
+        height={5000}
+        fill={false}
+        quality={100}
+      />
+    </div>
+  );
+}
