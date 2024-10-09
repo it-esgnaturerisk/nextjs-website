aecf642601f414091d835f7433d01291d5d6a0ed
hallvaeb <it@esgnaturerisk.com>
2024-10-09 19:37:15 +0200

    remove old way of loading env variables (?)

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,18 @@
-import "@/lib/config";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
-import {users} from "./schema";
+import { users } from "./schema";
 import * as schema from "./schema";
 
-export const db = drizzle(sql, {schema});
+export const db = drizzle(sql, { schema });
 
 export const getUsers = async () => {
-    const selectedResults = await db.select().from(users);
-    console.log("Results:", selectedResults);
-    return selectedResults;
-}
+  const selectedResults = await db.select().from(users);
+  console.log("Results:", selectedResults);
+  return selectedResults;
+};
 
 export type NewUser = typeof users.$inferInsert;
 
 export const insertUser = async (user: NewUser) => {
-     return db.insert(users).values(user).returning(); 
-}
+  return db.insert(users).values(user).returning();
+};
