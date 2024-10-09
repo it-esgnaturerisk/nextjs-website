48ab730251ddd4581b929b0bc2d72de59b587c25
hallvaeb <it@esgnaturerisk.com>
2024-10-09 20:20:35 +0200

    add aria-label and role

--- components/new-site-button.tsx
+++ components/new-site-button.tsx
@@ -14,6 +14,11 @@ const NewSiteButton = ({
   return (
     <Link
       href={href ? href : "/new-site"}
+      aria-label={
+        href
+          ? href.substring(1, href.length) + "-button"
+          : "button-label-no-href"
+      }
       className={`${classNameArgs} bg-greendark text-white py-2 px-4 m-2 rounded-lg shadow-md`}
     >
       {children ? children : "+ New Site"}
