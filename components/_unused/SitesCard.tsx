6cad6c48e712774387463f4e08f73454501df735
hallvaeb <it@esgnaturerisk.com>
2024-10-14 07:36:45 +0200

    comment out unused code

--- components/_unused/SitesCard.tsx
+++ components/_unused/SitesCard.tsx
@@ -1,47 +1,47 @@
-import React from "react";
-import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
-import { Button } from "../ui/button";
-import { SiteType } from "@/lib/types";
+// import React from "react";
+// import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+// import { Button } from "../ui/button";
+// import { SiteType } from "@/lib/types";
 
-const SiteCard = (site: SiteType) => {
-  const [removed, setRemoved] = React.useState(false);
+// const SiteCard = (site: SiteType) => {
+//   const [removed, setRemoved] = React.useState(false);
 
-  const handleClick = () => {
-    setRemoved(true);
-    // onRemove(site.key); // Uncomment this if you want to trigger the onRemove prop
-  };
+//   const handleClick = () => {
+//     setRemoved(true);
+//     // onRemove(site.key); // Uncomment this if you want to trigger the onRemove prop
+//   };
 
-  return (
-    <div>
-      {removed ? null : (
-        <Card className="relative">
-          {/* {site.imageurl ? (
-                        <Image alt="site" src={site.imageurl} className="rounded-md" /> */}
-          {/* ) : null} */}
-          <CardHeader>
-            <CardTitle>{site.name}</CardTitle>
-            <CardDescription>
-              Latitude: {site.latitude ? site.latitude.toFixed(4) : "N/A"}
-            </CardDescription>
-            <CardDescription>
-              Longitude: {site.longitude ? site.longitude.toFixed(4) : "N/A"}
-            </CardDescription>
-            <CardDescription>Address: {site.address}</CardDescription>
-          </CardHeader>
-          <div className="flex justify-end pb-1 px-1">
-            <Button size={"sm"}>View Details</Button>
-          </div>
-          {/* X button */}
-          <button
-            onClick={handleClick}
-            className="absolute top-1 right-1 text-black rounded-full p-1 text-xs"
-          >
-            X
-          </button>
-        </Card>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {removed ? null : (
+//         <Card className="relative">
+//           {/* {site.imageurl ? (
+//                         <Image alt="site" src={site.imageurl} className="rounded-md" /> */}
+//           {/* ) : null} */}
+//           <CardHeader>
+//             <CardTitle>{site.name}</CardTitle>
+//             <CardDescription>
+//               Latitude: {site.latitude ? site.latitude.toFixed(4) : "N/A"}
+//             </CardDescription>
+//             <CardDescription>
+//               Longitude: {site.longitude ? site.longitude.toFixed(4) : "N/A"}
+//             </CardDescription>
+//             <CardDescription>Address: {site.address}</CardDescription>
+//           </CardHeader>
+//           <div className="flex justify-end pb-1 px-1">
+//             <Button size={"sm"}>View Details</Button>
+//           </div>
+//           {/* X button */}
+//           <button
+//             onClick={handleClick}
+//             className="absolute top-1 right-1 text-black rounded-full p-1 text-xs"
+//           >
+//             X
+//           </button>
+//         </Card>
+//       )}
+//     </div>
+//   );
+// };
 
-export default SiteCard;
+// export default SiteCard;
--- components/_unused/SitesDisplay.tsx
+++ components/_unused/SitesDisplay.tsx
@@ -1,127 +1,127 @@
-import { SiteType } from "@/lib/types";
-import { SiteMarkerType } from "@/lib/types";
-import { useState } from "react";
+// import { SiteType } from "@/lib/types";
+// import { SiteMarkerType } from "@/lib/types";
+// import { useState } from "react";
 
-export default function SitesDisplay({
-  markers,
-}: {
-  markers: SiteMarkerType[];
-}) {
-  const [sites, setSites] = useState<SiteType[]>([]);
+// export default function SitesDisplay({
+//   markers,
+// }: {
+//   markers: SiteMarkerType[];
+// }) {
+//   const [sites, setSites] = useState<SiteType[]>([]);
 
-  const onRemove = (id: number) => {
-    const newSites = sites.filter((site) => site.id !== id);
-    setSites(newSites);
-  };
+//   const onRemove = (id: number) => {
+//     const newSites = sites.filter((site) => site.id !== id);
+//     setSites(newSites);
+//   };
 
-  const handleSitesRequest = () => {
-    return;
-  };
+//   const handleSitesRequest = () => {
+//     return;
+//   };
 
-  return (
-    <div className="flex flex-col">
-      <div className="grid grid-cols-1 w-[250px] gap-1 overflow-auto">
-        {sites.length === 0 ? (
-          <p className="text-center my-5">No existing sites found.</p>
-        ) : (
-          "Sites found, but functionality not implemented yet."
-          // sites.map((site) => (
-          //   <SiteCard {...site}/>
-          // ))
-        )}
-      </div>
-      <div>
-        <button
-          onClick={handleSitesRequest}
-          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Send til Tom
-        </button>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="flex flex-col">
+//       <div className="grid grid-cols-1 w-[250px] gap-1 overflow-auto">
+//         {sites.length === 0 ? (
+//           <p className="text-center my-5">No existing sites found.</p>
+//         ) : (
+//           "Sites found, but functionality not implemented yet."
+//           // sites.map((site) => (
+//           //   <SiteCard {...site}/>
+//           // ))
+//         )}
+//       </div>
+//       <div>
+//         <button
+//           onClick={handleSitesRequest}
+//           className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+//         >
+//           Send til Tom
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
 
-// import site1 from "@/public/_MG_0800.jpg";
-// import site2 from "@/public/_MG_1813.jpg";
-// import site3 from "@/public/_MG_1830.jpg";
-// import site4 from "@/public/_MG_2168.jpg";
-// import site5 from "@/public/_MG_2241.jpg";
-// import site6 from "@/public/_MG_2337.jpg";
-// import site7 from "@/public/_MG_2376.jpg";
-// import site8 from "@/public/_MG_2395.jpg";
-// import site9 from "@/public/_MG_2853.jpg";
-// import site10 from "@/public/_MG_2984.jpg";
+// // import site1 from "@/public/_MG_0800.jpg";
+// // import site2 from "@/public/_MG_1813.jpg";
+// // import site3 from "@/public/_MG_1830.jpg";
+// // import site4 from "@/public/_MG_2168.jpg";
+// // import site5 from "@/public/_MG_2241.jpg";
+// // import site6 from "@/public/_MG_2337.jpg";
+// // import site7 from "@/public/_MG_2376.jpg";
+// // import site8 from "@/public/_MG_2395.jpg";
+// // import site9 from "@/public/_MG_2853.jpg";
+// // import site10 from "@/public/_MG_2984.jpg";
 
-// const siteList=[
-//     {
-//         id: 1,
-//         image: site1,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     },
-//     {
-//         id: 2,
-//         image: site2,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     },
-//     {
-//         id: 3,
-//         image: site3,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     },
-//     {
-//         id: 4,
-//         image: site4,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     },
-//     {
-//         id: 5,
-//         image: site5,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     },
-//     {
-//         id: 6,
-//         image: site6,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     },
-//     {
-//         id: 7,
-//         image: site7,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     },
-//     {
-//         id: 8,
-//         image: site8,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     },
-//     {
-//         id: 9,
-//         image: site9,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     },
-//     {
-//         id: 10,
-//         image: site10,
-//         name: "Tittel på utslippsområdet.",
-//         siteStats: "Grad av miljøpåvirkning.",
-//         address: "Adressen."
-//     }
-// ];
+// // const siteList=[
+// //     {
+// //         id: 1,
+// //         image: site1,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     },
+// //     {
+// //         id: 2,
+// //         image: site2,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     },
+// //     {
+// //         id: 3,
+// //         image: site3,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     },
+// //     {
+// //         id: 4,
+// //         image: site4,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     },
+// //     {
+// //         id: 5,
+// //         image: site5,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     },
+// //     {
+// //         id: 6,
+// //         image: site6,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     },
+// //     {
+// //         id: 7,
+// //         image: site7,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     },
+// //     {
+// //         id: 8,
+// //         image: site8,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     },
+// //     {
+// //         id: 9,
+// //         image: site9,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     },
+// //     {
+// //         id: 10,
+// //         image: site10,
+// //         name: "Tittel på utslippsområdet.",
+// //         siteStats: "Grad av miljøpåvirkning.",
+// //         address: "Adressen."
+// //     }
+// // ];
