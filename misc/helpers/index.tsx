5a1f650dbf01a426f449521beca0e8a1bdde6b62
hallvaeb <it@esgnaturerisk.com>
2024-11-10 19:31:49 +0100

    eslint error corrections

--- misc/helpers/index.tsx
+++ misc/helpers/index.tsx
@@ -1,10 +1,11 @@
 import * as turf from '@turf/turf';
-import { CircleOptions } from '@turf/turf';
+import { Units } from '@turf/helpers';
 
 // eslint-disable-next-line import/prefer-default-export
 export function createCircle(lng: number, lat: number, radiusInKm: number) {
   const center = [lng, lat];
-  const options: CircleOptions = { steps: 64, units: 'kilometers' };
+  const units: Units = 'kilometers';
+  const options = { steps: 64, units: units };
 
   const circle = turf.circle(center, radiusInKm, options);
 
@@ -14,6 +15,10 @@ export function createCircle(lng: number, lat: number, radiusInKm: number) {
 export function formatDateLocale(dateString: Date | null) {
   if (!dateString) return null;
   const date = new Date(dateString);
-  const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'long', year: 'numeric' };
+  const options: Intl.DateTimeFormatOptions = {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+  };
   return date.toLocaleDateString('en-GB', options);
 }
