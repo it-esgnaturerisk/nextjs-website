56942f6c2dee3530acc7a9314d01a7a2a5676cc7
hallvaeb <it@esgnaturerisk.com>
2024-11-25 21:39:29 +0100

    Button component generalized

---
+++ components/buttons/RemoveSiteButton.tsx
@@ -0,0 +1,17 @@
+// ''
+
+// import { removeSite } from "@/lib/db/queries";
+// import { TrashIcon } from "@radix-ui/react-icons";
+// import Link from "next/link";
+// import Button from "./Button";
+
+
+// function handleRemoveSite (uuid: string) {
+    
+// }
+
+
+// export default function RemoveSiteButton({uuid} : {uuid:string}){
+    
+//     return <TrashIcon onClick={handleRemoveSite(uuid)}></TrashIcon>
+// }
