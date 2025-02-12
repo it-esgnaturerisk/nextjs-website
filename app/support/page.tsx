f6f355df6836e159eca8707a4c103aff4561597c
hallvaeb <it@esgnaturerisk.com>
2025-02-12 20:31:10 +0100

    add support page

---
+++ app/support/page.tsx
@@ -0,0 +1,5 @@
+export default function Support(){
+    return (
+        <h1>Not yet implemented. Please contact tom@esgnaturerisk.com regarding your concern and we'll do our best to meet your needs!</h1>
+    )
+}
