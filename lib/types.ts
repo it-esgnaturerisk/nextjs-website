8c00adf82196a93b94e62e8252df330d5290b7f1
hallvaeb <it@esgnaturerisk.com>
2025-04-14 20:39:09 +0200

    extend to match db

--- lib/types.ts
+++ lib/types.ts
@@ -4,6 +4,13 @@ import {
   portfolios,
   ranges,
   companies,
+  species,
+  speciesObserved,
+  siteRanges,
+  institutions,
+  institutionsObservations,
+  yearsOfObservations,
+  speciesGroups,
 } from './db/schema';
 
 export type UserType = typeof users.$inferSelect;
@@ -14,6 +21,15 @@ export type NewPortfolioType = typeof portfolios.$inferInsert;
 export type PortfolioType = typeof portfolios.$inferSelect;
 export type RangesType = typeof ranges.$inferSelect;
 export type CompaniesType = typeof companies.$inferSelect;
+export type SpeciesType = typeof species.$inferSelect;
+export type SpeciesObservedType = typeof speciesObserved.$inferSelect;
+export type SpeciesObservedInsertType = typeof speciesObserved.$inferInsert;
+export type SpeciesGroupType = typeof speciesGroups.$inferSelect;
+export type InstitutionsType = typeof institutions.$inferSelect;
+export type InstitutionsObservationsType = typeof institutionsObservations.$inferSelect;
+export type YearsOfObservationsType = typeof yearsOfObservations.$inferSelect;
+export type SiteRangeType = typeof siteRanges.$inferSelect;
+export type SiteRangeInsertType = typeof siteRanges.$inferInsert;
 
 export type SiteMarkerType = {
   lng: number;
